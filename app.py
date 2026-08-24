87ca2eab776054c68dd52d5c4f423231ff115a43
shubham <shubhamndeshmukh02@gmail.com>
2026-08-24 12:24:25 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+# Database Configuration
+db_config = {
+    'host': 'database-1.cex28aeu05yi.us-east-1.rds.amazonaws.com',
+    'user': 'admin',
+    'password': 'admin123',
+    'database': 'dev'
+}
